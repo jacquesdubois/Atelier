caa21265a282132e71ac89562b04c246e71fa89d
jacquesdubois <jacquesadubois@gmail.com>
2021-09-28 14:26:39 -0400

    Refactored mongoModels, added additional validation checks to schema.sql, created psql db

--- db/mongoModels.js
+++ db/mongoModels.js
@@ -1,51 +1,46 @@
 const { Schema } = require('mongoose');
 
 const productSchema = new Schema({
-    id: {type: Number, unique: true},
+    _id: {type: Number, unique: true},
     campus: String,
     name: {type: String, unique: true},
     slogan: String,
     description: String,
+    feature: [{ feature: String, value: String }],
+    related: [Number], // maybe make this an array of product objects
     category: String,
     default_price: String,
     created_at: String,
-    updated_at: String
-});
-
-const featureSchema = new Schema({
-    id: {type: Number, unique: true},
-    feature: String,
-    value: String,
-    product_id: Number
+    styles:[Number] // potentially include full Style object later on
 });
 
 const styleSchema = new Schema({
-    id: {type: Number, unique: true},
+    _id: {type: Number, unique: true},
     name: String,
     original_price: String,
     sale_price: String,
     'default?': Boolean,
-    product_id: Number
+    product_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+    photos: [{ url: String, thumbnail_url: String }],
+    skus: [Number]
 });
 
+// Nested Into Styles, but also kept separate for rapid inventory updates
 const skuSchema = new Schema({
-    id: {type: Number, unique: true},
+    _id: {type: Number, unique: true},
     quantity: Number,
     size: String,
-    style_id: Number
-});
-
-const photoSchema = new Schema({
-    id: {type: Number, unique: true},
-    url: String,
-    thumbnail_url: String,
-    style_id: Number
+    style_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Style'
+    }
 });
 
 module.exports = {
     Product: mongoose.model('Product', productSchema, 'CatwalkOverview'),
-    Feature: mongoose.model('Feature', featureSchema, 'CatwalkOverview'),
     Style: mongoose.model('Style', styleSchema, 'CatwalkOverview'),
     Sku: mongoose.model('Sku', skuSchema, 'CatwalkOverview'),
-    Photo: mongoose.model('Photo', photoSchema, 'CatwalkOverview')
 };
--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,38 @@
 -- DROP DATABASE IF EXISTS test;
-CREATE DATABASE CatwalkOverview;
+-- CREATE DATABASE CatwalkOverview;
 
-\c CatwalkOverview
+-- \c CatwalkOverview
+
+-- Add more not nulls
+-- Add additional validation
 
 CREATE TABLE products(
     id INT UNIQUE NOT NULL,
-    campus VARCHAR(30),
     name VARCHAR(30) UNIQUE NOT NULL,
-    slogan VARCHAR(50),
-    description VARCHAR(250),
+    slogan VARCHAR(50) NOT NULL,
+    description VARCHAR(250) NOT NULL,
     category VARCHAR(30) NOT NULL,
-    default_price VARCHAR(30),
-    created_at VARCHAR(30),
-    updated_at VARCHAR(30),
+    default_price INT NOT NULL CHECK (default_price > 0),
+    created_at VARCHAR(30) NOT NULL,
+    updated_at VARCHAR(30) NOT NULL CHECK (updated_at > created_at),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE features(
     id INT UNIQUE NOT NULL,
-    feature VARCHAR(30),
-    value VARCHAR(30),
-    product_id INT,
+    feature VARCHAR(30) NOT NULL,
+    value VARCHAR(30) NOT NULL,
+    product_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id)
+        REFERENCES products (id)
+);
+
+CREATE TABLE related(
+    id INT UNIQUE NOT NULL,
+    product_id INT NOT NULL,
+    related_id INT NOT NULL,
+    -- orderVar INT,
     PRIMARY KEY (id),
     FOREIGN KEY (product_id)
         REFERENCES products (id)
@@ -29,30 +41,28 @@ CREATE TABLE features(
 CREATE TABLE styles(
     id INT UNIQUE NOT NULL,
     name VARCHAR(30) NOT NULL,
-    original_price VARCHAR(30) NOT NULL,
-    sale_price VARCHAR(30),
-    `default?` BOOLEAN,
-    product_id INT,
+    original_price INT NOT NULL CHECK (original_price > sale_price AND original_price > 0),
+    sale_price INT CHECK (sale_price = null OR sale_price < original_price),
+    "default?" BOOLEAN NOT NULL,
+    product_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (product_id)
-        REFERENCES products (id)
+    FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
 CREATE TABLE skus(
     id INT UNIQUE NOT NULL,
     quantity INT NOT NULL,
-    size VARCHAR(10),
+    size VARCHAR(10) NOT NULL,
     style_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (style_id)
-        REFERENCES styles (id)
+    FOREIGN KEY (style_id) REFERENCES styles (id)
 );
 
 CREATE TABLE photos(
     id INT UNIQUE NOT NULL,
-    url VARCHAR(200),
-    thumbnail_url VARCHAR(200),
-    style_id INT,
+    url VARCHAR(200) NOT NULL,
+    thumbnail_url VARCHAR(200) NOT NULL,
+    style_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (style_id)
         REFERENCES styles (id)
